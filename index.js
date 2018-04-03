d8ff9a0cb6867a34e1e69b5246eb37711d60fce1
Lyubomir <16529989+mystiquewolf@users.noreply.github.com>
2018-04-03 15:46:39 +0300

    Fix bugs with pull request #1
    Moved most of the script code in index.html inside
    google.setOnLoadCallback to prevent some issues from appearing
    
    Renamed emmited events and some variables in index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var onlineUsers = 0;
-var doorCounter = 0;
+var doorOneCounter = 0;
 var doorTwoCounter = 0;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/sensor', function(req, res) {
-    doorCounter += Number(req.query.value);
-    io.emit('door', doorCounter);
-    res.send('OK:' + req.query.value);
+app.get('/sensor', function (req, res) {
+  doorOneCounter += Number(req.query.value);
+  io.emit('doorOne', doorOneCounter);
+  res.send('OK:' + req.query.value);
 });
 
-app.get('/sensortwo', function(req, res) {
-    doorTwoCounter += Number(req.query.value);
-    io.emit('door2', doorTwoCounter);
-    res.send('Door2 OK:' + req.query.value);
+app.get('/sensortwo', function (req, res) {
+  doorTwoCounter += Number(req.query.value);
+  io.emit('doorTwo', doorTwoCounter);
+  res.send('Door2 OK:' + req.query.value);
 });
 
 http.listen(3000, function () {
@@ -29,13 +29,13 @@ http.listen(3000, function () {
 
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  // console.log('a user connected');
   onlineUsers++;
-  io.emit('online', onlineUsers);
+  io.emit('onlineUserCount', onlineUsers);
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    // console.log('user disconnected');
     if (onlineUsers > 0)
       onlineUsers--;
-    io.emit('online', onlineUsers);
+    io.emit('onlineUserCount', onlineUsers);
   });
 });
