7408cdc5383215ca9eacbb3a65d6556defe25dc0
Lyubomir Parvanov <liubomirwm@users.noreply.github.com>
2018-04-29 14:16:35 +0300

    Add config file and change average people counters to use integers (#6)
    * Add config file
    Change var-declared variables to let
    Renamed "device" to "sensorId"
    
    * Change door one and door two average gauges to show passed people as integer values.

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.doorOneSensorId = "doorOneSensor";
+exports.doorTwoSensorId = "doorTwoSensor";
+exports.noiseSensorId = "noiseSensor";
+exports.airDustinessSensorId = "airDustinessSensor";
+exports.timeToCalculateAverageFor = 20; //in minutes
+exports.removeDustDataAfter = 43200000; //in milliseconds
+exports.port = 8080;
+exports.resetDoorTotalAfter = {
+    hour: 0,
+    minute: 0
+}
